c6917840e20b80d7ba66f8fad2331a1e52eb06b2
Dennis Fyrén <106529711+dennisfyren@users.noreply.github.com>
2025-03-18 20:05:34 +0100

    Written program for rock, paper and scissors
    Finished and tested.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rock-paper-scissors
+
+Rock paper scissor project.
+
+1. Make userinput through prompt.
+2. Make random number generator.
+3. Assign random number to rock paper or scissor.
+4. Compare userinput to see if they win.
+5. Announce result.
---
+++ java.js
@@ -0,0 +1,48 @@
+let userInput = prompt("Rock, paper or scissors?");
+
+userInput = userInput.toLowerCase();
+
+if (userInput === "rock"){
+    userInput = 1;
+    console.log("You chose Rock")
+} else if (userInput === "paper"){
+    userInput = 2;
+    console.log("You chose Paper")
+} else if (userInput === "scissors") {
+    userInput = 3;
+    console.log("You chose Scissors")
+} else {
+    console.log("Invalid choice, try again!");
+    alert("Invalid choice!")
+};
+
+let computerInput = Math.floor(Math.random() * 100);
+
+if (computerInput <= 33) {
+    computerInput = 1;
+    console.log("Computer chose Rock");
+} else if (computerInput >= 34 && computerInput <= 67) {
+    computerInput = 2;
+    console.log("Computer chose Paper");
+} else {
+    computerInput = 3;
+    console.log("Computer chose Scissors");
+};
+
+if (userInput === 1 && computerInput === 2) {
+    alert("You lose!");
+} else if(userInput === 1 && computerInput === 3) {
+    alert("You win!");
+} else if(userInput === 2 && computerInput === 3) {
+    alert("You lose!");
+} else if(userInput === 2 && computerInput === 1) {
+    alert("You win");
+} else if(userInput === computerInput) {
+    alert("It's a draw.");
+} else {
+    location.reload();
+}
+
+location.reload();
+
+console.log(computerInput);
